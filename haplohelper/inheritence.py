e1b864bd52d69aa2fb644e0aa89ffe99dfb2d2b6
Tim Millar <tim.millar@plantandfood.co.nz>
2019-07-03 14:32:06 +1200

    functions to determine genotype probabilities from a cross of haplotype sets

--- haplohelper/inheritence.py
+++ haplohelper/inheritence.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from itertools import combinations as _combinations
+from collections import Counter as _Counter
+import biovector
 from biovector import mset
 
 
@@ -87,3 +90,131 @@ class TrioChildInheritance(object):
             if len(dad_update) > 0:
                 self.dad_taken = mset.add(self.dad_taken, dad_update)
                 improving = True
+
+
+def _suggest_alphabet(vector_size):
+    if vector_size == 2:
+        return biovector.Biallelic
+    elif vector_size == 3:
+        return biovector.Triallelic
+    elif vector_size == 4:
+        return biovector.Quadraallelic
+    else:
+        raise ValueError(
+            'No suitable alphabet for vector size {}'.format(n_nucl))
+
+
+def gamete_probabilities(haplotype_sets,
+                         haplotype_set_probabilities,
+                         order=None,
+                         alphabet=None):
+    assert order in {None, 'ascending', 'descending'}
+
+    n_sets, ploidy, n_base, n_nucl = haplotype_sets.shape
+
+    if alphabet is None:
+        alphabet = _suggest_alphabet(n_nucl)
+
+    # convert haplotypes to strings for faster hashing/comparisons
+    string_to_hap = {}
+    string_sets = np.empty(n_sets * ploidy, dtype='<O')
+    for i, hap in enumerate(haplotype_sets.reshape(n_sets * ploidy,
+                                                   n_base,
+                                                   n_nucl)):
+        string = alphabet.decode(hap)
+        string_to_hap[string] = hap
+        string_sets[i] = string
+    string_sets = np.sort(string_sets.reshape(n_sets, ploidy), axis=-1)
+
+    # calculate gamete probs
+    gamete_probs = {}
+    for string_set, set_prob in zip(string_sets, haplotype_set_probabilities):
+        gametes = list(_combinations(string_set, ploidy // 2))
+        n_gametes = len(gametes)
+        for gamete, count in _Counter(gametes).items():
+            prob = set_prob * (count / n_gametes)
+            if gamete in gamete_probs:
+                gamete_probs[gamete] += prob
+            else:
+                gamete_probs[gamete] = prob
+
+    # convert from hash map of string: prob back to arrays
+    n_gametes = len(gamete_probs)
+    new = np.empty((n_gametes, ploidy // 2, n_base, n_nucl), dtype=np.int8)
+    new_probs = np.empty(n_gametes, dtype=np.float)
+    for i, (strings, prob) in enumerate(gamete_probs.items()):
+        new_probs[i] = prob
+        for j, string in enumerate(strings):
+            new[i, j] = string_to_hap[string]
+
+    if order:
+        idx = np.argsort(new_probs)
+        if order == 'descending':
+            idx = np.flip(idx, axis=0)
+        return new[idx], new_probs[idx]
+    else:
+        return new, new_probs
+
+
+def cross_probabilities(maternal_gametes,
+                        maternal_probabilities,
+                        paternal_gametes,
+                        paternal_probabilities,
+                        order=None,
+                        alphabet=None):
+
+    # get dimensions
+    half_ploidy, n_base, n_nucl = maternal_gametes.shape[-3:]
+    ploidy = half_ploidy * 2
+
+    # get alphabet
+    if alphabet is None:
+        alphabet = _suggest_alphabet(n_nucl)
+
+    # compute genotypes and probabilities
+    genotype_probs = {}
+    string_to_haplotype = {}
+    for i, (m_gamete, m_prob) in enumerate(zip(maternal_gametes,
+                                               maternal_probabilities)):
+        for j, (p_gamete, p_prob) in enumerate(zip(paternal_gametes,
+                                                   paternal_probabilities)):
+            genotype = np.empty(ploidy, dtype='<O')
+            idx = 0
+            for gamete in [m_gamete, p_gamete]:
+                for hap in gamete:
+                    string = alphabet.decode(hap)
+                    genotype[idx] = string
+                    idx += 1
+                    if string not in string_to_haplotype:
+                        string_to_haplotype[string] = hap
+            genotype = tuple(np.sort(genotype))
+            prob =  m_prob * p_prob
+            if genotype in genotype_probs:
+                genotype_probs[genotype] += prob
+            else:
+                genotype_probs[genotype] = prob
+
+    # convert genotypes bac to arrays
+    n_genotypes = len(genotype_probs)
+    new = np.empty((n_genotypes, ploidy, n_base, n_nucl), dtype=np.int8)
+    new_probs = np.empty(n_genotypes, dtype=np.float)
+
+    for i, (genotype, prob) in enumerate(genotype_probs.items()):
+        new_probs[i] = prob
+        for j, string in enumerate(genotype):
+            new[i,j] = string_to_haplotype[string]
+
+    if order:
+        idx = np.argsort(new_probs)
+        if order == 'descending':
+            idx = np.flip(idx, axis=0)
+        return new[idx], new_probs[idx]
+    else:
+        return new, new_probs
+
+
+
+
+
+
+
