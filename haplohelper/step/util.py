ca338603f8af267f7a6b18fb95dbee5c48b39b32
Timothy Millar <tim.millar@plantandfood.co.nz>
2019-09-30 12:04:24 +1300

    update step util.py

--- haplohelper/step/util.py
+++ haplohelper/step/util.py
@@ -3,19 +3,82 @@ from numba import jit
 
 @jit(nopython=True)
 def sum_log_prob(x, y):
+    """Sum of probabilities in log space.
+
+    Parameters
+    ----------
+    x : float
+        A log-transformed probability.
+    y : float
+        A log-transformed probability.
+
+    Returns
+    -------
+    z : The log-transformed sum of the un-transformed `x` and `y`.
+
+    """
     if x > y:
         return x + np.log1p(np.exp(y - x))
     else:
         return y + np.log1p(np.exp(x - y))
 
+
 @jit(nopython=True)
-def rand_choice(arr, prob):
-    return arr[np.searchsorted(np.cumsum(prob), np.random.random(), side="right")]
+def random_choice(options, probabilities):
+    """Random choice of options given a set of probabilities.
+
+    Parameters
+    ----------
+    options : array_like, int
+        An array of options to choose from.
+    probabilities : array_like, float
+        An array of probabilities asociated with each value in `options`.
+
+    Returns
+    -------
+    choice : int
+        A value from `options` selected at random with using `probabilities`.
+
+    """
+    return options[np.searchsorted(np.cumsum(probabilities), np.random.random(), side="right")]
+
+
+@jit(nopython=True)
+def greedy_choice(options, probabilities):
+    """Greedy choice of options given a set of probabilities.
+
+    Parameters
+    ----------
+    options : array_like, int
+        An array of options to choose from.
+    probabilities : array_like, float
+        An array of probabilities asociated with each value in `options`.
+
+    Returns
+    -------
+    choice : int
+        The value in `options` with the largest corresponding value in `probabilities`.
+
+    """
+    return options[np.argmax(probabilities)]
 
 
 @jit(nopython=True)
 def array_equal(x, y):
-    """Assumes equal length and dtype.
+    """Check if two one-dimentional integer arrays are equal.
+
+    Parameters
+    ----------
+    x : array_like, int
+        A one-dimentional array of integers.
+    y : array_like, int
+        A one-dimentional array of integers with the same length as `x`.
+
+    Returns
+    -------
+    equality : bool
+        True if `x` and `y` are equal, else False.
+
     """
     n = len(x)
     for i in range(n):
@@ -25,30 +88,45 @@ def array_equal(x, y):
 
 
 @jit(nopython=True)
-def calculate_dosage(integer_haplotypes, dosage_array):
+def genotype_dosage(genotype, dosage):
     """Calculates the dosage of a set of integer encoded haplotypes by 
-    checking for array identity.
+    checking for array equality.
     
-    Dosages are set in the pssed array.
-    A dosage of 0 indicates that that haplotype is a duplicate of a previous haplotype
+    Parameters
+    ----------
+    genotype : array_like, int, shape (ploidy, n_base)
+        Initial state of haplotypes with base positions encoded as simple integers from 0 to n_nucl.
+    dosage : array_like, int, shape (ploidy)
+        Array to update with dosage of each haplotype.
+
+    Returns
+    -------
+    None
+    
+    Notes
+    -----
+    The `dosage` variable is updated in place.
+    The `dosage` array should always sum to the number of haplotypes in the `genotype`.
+    A value of `0` in the `dosage` array indicates that that haplotype is a duplicate of another.
+
     """
     # start with assumption that all are unique
-    dosage_array[:] = 1
+    dosage[:] = 1
     
-    ploidy, n_base = integer_haplotypes.shape
+    ploidy, _ = genotype.shape
     
     for h in range(ploidy):
-        if dosage_array[h] == 0:
+        if dosage[h] == 0:
             # this haplotype has already been identified as equal to another
             pass
         else:
             # iterate through remaining haps
             for p in range(h+1, ploidy):
-                if dosage_array[p] == 0:
+                if dosage[p] == 0:
                     # this haplotype has already been identified as equal to another
                     pass
                 else:
-                    if util.array_equal(integer_haplotypes[h], integer_haplotypes[p]):
-                        dosage_array[h] += 1
-                        dosage_array[p] = 0
+                    if array_equal(genotype[h], genotype[p]):
+                        dosage[h] += 1
+                        dosage[p] = 0
                         
