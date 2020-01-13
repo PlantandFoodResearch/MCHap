ca1d1b4f98ff0590d891aa3e1b164159dcb33a82
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-01-14 11:57:21 +1300

    complexity functions

--- haplohelper/util.py
+++ haplohelper/util.py
@@ -3,6 +3,8 @@
 import numpy as np
 from itertools import islice as _islice
 from itertools import zip_longest as _zip_longest
+from math import factorial as _factorial
+
 import biovector
 
 
@@ -38,11 +40,38 @@ def middle_out(sequence):
 
 def suggest_alphabet(vector_size):
     if vector_size == 2:
-        return biovector.Biallelic
+        return biovector.alphabet.biallelic
     elif vector_size == 3:
-        return biovector.Triallelic
+        return biovector.alphabet.triallelic
     elif vector_size == 4:
-        return biovector.Quadraallelic
+        return biovector.alphabet.quadraallelic
     else:
         raise ValueError(
             'No suitable alphabet for vector size {}'.format(vector_size))
+
+
+def count_unique_haplotypes(n_base, n_nucl):
+    return n_base ** n_nucl
+
+
+def count_unique_genotypes(u_haps, ploidy):
+    return _factorial(u_haps + ploidy -1) // (_factorial(ploidy) * _factorial(u_haps-1))
+
+
+def count_haplotype_universial_occurance(u_haps, ploidy):
+    """Counts the number of ocurances of a haplotype among all possible unique genotypes.
+    """
+    return _factorial(u_haps + ploidy -1) // (_factorial(ploidy-1) * _factorial(u_haps))
+
+
+def count_genotype_perterbations(dosage):
+    """Counts the total number of equivilent genotype perterbation based on the dosage.
+
+    A genotype is an unsorted set of haplotypes hence the genotype `{A, B}` is equivielnt
+    to the genotype `{B, A}`.
+    A fully homozygous genotype e.g. `{A, A}` has only one possible perterbation.
+    """
+    ploidy = np.sum(dosage)
+    numerator = _factorial(ploidy)
+    denominator = np.prod([_factorial(d) for d in dosage])
+    return numerator // denominator
