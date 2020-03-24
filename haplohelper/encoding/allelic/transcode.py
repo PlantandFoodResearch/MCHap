082c39e5c51ba3ecea51e82d9348dfbfdcce6c6d
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-03-24 19:53:18 +1300

    encode and decode from string

--- haplohelper/encoding/allelic/transcode.py
+++ haplohelper/encoding/allelic/transcode.py
@@ -81,4 +81,80 @@ def as_probabilistic(array, n_alleles, p=1.0, vector_size=None, gap_as_nan=True,
     return new.reshape(shape)
 
     
+def vector_from_string(string, gaps='-', length=None, dtype=np.int8):
+    if length is None:
+        length = len(string)
+
+    # gap is default
+    vector = np.zeros(length, dtype=dtype) - 1
+
+    for i in range(min(length, len(string))):
+        char = string[i]
+        if char in gaps:
+            vector[i] = -1
+        else:
+            vector[i] = int(char)
+    return vector
+
+
+def from_strings(data, gaps='-', length=None, dtype=np.int8):
+    if isinstance(data, str):
+        return vector_from_string(data, gaps=gaps, length=length, dtype=dtype)
+    
+    if isinstance(data, np.ndarray):
+        pass
+    else:
+        data = np.array(data, copy=False)
+
+    sequences = data.ravel()
+
+    # default to length of longest element
+    if length is None:
+        length =  max(len(i) for i in sequences)
+
+    # number of sequences
+    n_seq = len(sequences)
+
+    # new array with gap as default
+    array = np.empty((n_seq, length), dtype=dtype)
+
+    for i in range(n_seq):
+        array[i] = vector_from_string(
+            sequences[i],
+            gaps=gaps,
+            length=length,
+            dtype=dtype
+        )
+
+    shape = data.shape + (length, )
+
+    return array.reshape(shape)
+
+
+def vector_as_string(vector, gap='-'):
+    return ''.join(str(i) if i >= 0 else gap for i in vector)
+
+
+def as_strings(array, gap='-'):
+    if not isinstance(array, np.ndarray):
+        array = np.array(array, copy=False)
+    if array.ndim == 1:
+        return vector_as_string(array, gap=gap)
+
+    shape = array.shape[:-1]
+    length = array.shape[-1]
+    dtype = 'U{}'.format(length)
+    n_seq = np.prod(shape)
+    vectors = array.reshape(n_seq, length)
+
+    strings = np.empty(n_seq, dtype=dtype)
+
+    for i in range(n_seq):
+        strings[i] = vector_as_string(vectors[i], gap=gap)
+
+    return strings.reshape(shape)
+
+
+
+
     
