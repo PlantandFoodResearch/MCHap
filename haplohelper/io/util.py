f5b73c66058c7abd14adeb7c594bb5855a3b9070
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-04-15 08:59:11 +1200

    add prcision arg to qual_of_prob

--- haplohelper/io/util.py
+++ haplohelper/io/util.py
@@ -16,5 +16,22 @@ def prob_of_qual(qual):
     return 1 - (10 ** (qual / -10))
 
 
-def qual_of_prob(prob):
-    return int(-10 * np.log10((1 - prob)))
+def qual_of_prob(prob, precision=6):
+    # cant have a prob of 1 converted to a qual
+    # instead convert based on the expected decimal precision of the prob
+    # a precision of 6 produces a max qual of 60
+    maximum = 1 - 0.1 ** precision
+
+    if np.shape(prob) == ():
+        # prob is scalar
+        if prob > maximum:
+            prob = maximum
+        else:
+            pass
+
+    else:
+        # prob is array-like
+        prob = np.array([maximum if p > maximum else p for p in prob])
+        
+    prob = np.floor(prob * 10**precision) / 10**precision
+    return np.round((-10 * np.log10((1 - prob)))).astype(np.int)
