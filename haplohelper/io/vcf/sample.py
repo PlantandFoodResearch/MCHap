858cbee2e0e1fc93b3da85a6601b3555281308c2
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-04-22 17:27:19 +1200

    clean up filter code

--- haplohelper/io/vcf/sample.py
+++ haplohelper/io/vcf/sample.py
@@ -2,7 +2,7 @@ import numpy as np
 from dataclasses import dataclass
 
 from haplohelper import mset
-from haplohelper.encoding import allelic
+from haplohelper.encoding import allelic, symbolic
 from haplohelper.io.bam import dtype_allele_call as _dtype_allele_call
 
 _PASS_CODE = 'PASS'
@@ -12,16 +12,12 @@ _DEPTH_CODE = 'd<{threshold}'
 _PROB_CODE = 'p<{threshold}'
 
 
-def kmer_representation(read_calls, haplotype_calls, k=3):
+def kmer_representation(variants, haplotype_calls, k=3):
     """ Calculates position-wise frequency of read_calls kmers which 
     are also present in haplotype_calls.
     """
-    if read_calls.dtype == _dtype_allele_call:
-        # we only need the allele calls not the quals
-        read_calls = read_calls['allele']
-
     # create kmers and counts
-    read_kmers, read_kmer_counts = allelic.kmer_counts(read_calls, k=k)
+    read_kmers, read_kmer_counts = allelic.kmer_counts(variants, k=k)
     hap_kmers, _ = allelic.kmer_counts(haplotype_calls, k=k)
     
     # index of kmers not found in haplotypes
@@ -40,31 +36,25 @@ def kmer_representation(read_calls, haplotype_calls, k=3):
     return result
 
 
-def kmer_variant_filter(read_calls, genotype, k=3, threshold=0.95):
-    n_pos = read_calls.shape[-1]
+def kmer_variant_filter(variants, genotype, k=3, threshold=0.95):
+    n_pos = variants.shape[-1]
     if n_pos < k:
         # can't apply kmer filter
         return [_NULL_CODE for _ in range(n_pos)]
 
-    if read_calls.dtype == _dtype_allele_call:
-        # we only need the allele calls not the quals
-        read_calls = read_calls['allele']
-    freqs = kmer_representation(read_calls, genotype, k=k)
+    freqs = kmer_representation(variants, genotype, k=k)
     fails = freqs < threshold
     code = _KMER_CODE.format(k=k, threshold=threshold)
     return [code if fail else _PASS_CODE for fail in fails]
 
 
-def kmer_haplotype_filter(read_calls, genotype, k=3, threshold=0.95):
-    n_pos = read_calls.shape[-1]
+def kmer_haplotype_filter(variants, genotype, k=3, threshold=0.95):
+    n_pos = variants.shape[-1]
     if n_pos < k:
         # can't apply kmer filter
         return _NULL_CODE
 
-    if read_calls.dtype == _dtype_allele_call:
-        # we only need the allele calls not the quals
-        read_calls = read_calls['allele']
-    freqs = kmer_representation(read_calls, genotype, k=k)
+    freqs = kmer_representation(variants, genotype, k=k)
     fail = np.any(freqs < threshold)
     if fail:
         return _KMER_CODE.format(k=k, threshold=threshold)
@@ -72,22 +62,14 @@ def kmer_haplotype_filter(read_calls, genotype, k=3, threshold=0.95):
         return _PASS_CODE
 
 
-def depth_variant_filter(read_calls, threshold=5.0):
-    if read_calls.dtype == _dtype_allele_call:
-        # we only need the allele calls not the quals
-        read_calls = read_calls['allele']
-    depth = allelic.depth(read_calls)
-    fails = depth < threshold
+def depth_variant_filter(depths, threshold=5.0, gap='-'):
+    fails = depths < threshold
     code = _DEPTH_CODE.format(threshold=threshold)
     return [code if fail else _PASS_CODE for fail in fails]
 
 
-def depth_haplotype_filter(read_calls, threshold=5.0):
-    if read_calls.dtype == _dtype_allele_call:
-        # we only need the allele calls not the quals
-        read_calls = read_calls['allele']
-    depth = allelic.depth(read_calls)
-    fail = np.mean(depth) < threshold
+def depth_haplotype_filter(depths, threshold=5.0, gap='-'):
+    fail = np.mean(depths) < threshold
     if fail:
         return _DEPTH_CODE.format(threshold=threshold)
     else:
