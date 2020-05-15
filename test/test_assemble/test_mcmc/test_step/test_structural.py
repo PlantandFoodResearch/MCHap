e788815d7e0743bd7b3335cb24e061b68c13061d
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-05-15 16:07:52 +1200

    test_recombination_step_options

--- test/test_assemble/test_mcmc/test_step/test_structural.py
+++ test/test_assemble/test_mcmc/test_step/test_structural.py
@@ -15,6 +15,8 @@ from haplohelper.assemble.mcmc.step import structural
 ])
 def test_random_breaks(breaks, n):
 
+    # intervals are of random length but should be within several constraints
+    # so test that they 
     intervals = structural.random_breaks(breaks, n)
 
     # number of intervals produced is 1 + the break points between intervals
@@ -25,7 +27,7 @@ def test_random_breaks(breaks, n):
     assert np.all(lengths > 0)
 
     # all positions must be contained in one and only one interval
-    # i.e. the end of one interval must be the start of the next
+    # i.e. the end position of one interval must also be the start of the next
 
     # check first interval starts at 0
     assert intervals[0, 0] == 0
@@ -150,3 +152,47 @@ def test_haplotype_segment_labels(genotype, interval, answer):
 
     np.testing.assert_array_equal(query, answer)
 
+
+@pytest.mark.parametrize('labels,answer', [
+    pytest.param(
+        [[0, 0], [1, 0]],
+        np.empty((0, 2), dtype=np.int8),  # no options
+        id='2x-0'),
+    pytest.param(
+        [[0, 0], [1, 1]],
+        [[1, 0]],  # 1 option
+        id='2x-1'),
+    pytest.param(
+        [[0, 0], [0, 1], [0, 1], [0, 0]],
+        np.empty((0, 4), dtype=np.int8),  # no options
+        id='4x-0'),
+    pytest.param(
+        [[0, 0], [0, 1], [2, 1], [0, 0]],
+        [[2, 1, 0, 3]],  # 1 option
+        id='4x-1'),
+    pytest.param(
+        [[0, 0], [0, 1], [2, 1], [3, 0]],
+        [[2, 1, 0, 3],
+         [0, 3, 2, 1],
+         [0, 1, 3, 2]],  # 3 options
+        id='4x-3'),
+    pytest.param(
+        [[0, 0], [1, 1], [2, 2], [3, 3]],
+        [[1, 0, 2, 3],
+         [2, 1, 0, 3],
+         [3, 1, 2, 0],
+         [0, 2, 1, 3],
+         [0, 3, 2, 1],
+         [0, 1, 3, 2]],  # all the options
+        id='4x-6'),
+])
+def test_recombination_step_options(labels, answer):
+
+    labels = np.array(labels)
+    answer = np.array(answer)
+
+    n_options = structural.recombination_step_n_options(labels)
+    query = structural.recombination_step_options(labels)
+
+    assert len(query) == n_options
+    np.testing.assert_array_equal(query, answer)
