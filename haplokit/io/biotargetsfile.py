17f4fe79074e9809b69ee5ae4c6aad74fe5b3a77
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-06-15 16:23:51 +1200

    support pedigree meta fields

--- haplokit/io/biotargetsfile.py
+++ haplokit/io/biotargetsfile.py
@@ -5,11 +5,21 @@ from dataclasses import dataclass
 
 meta_pattern = re.compile(r'''\#\#\s*(?P<id>[^,]+)\s*=\s*(?P<descr>[^,]+)\s*''')
 
-column_pattern = re.compile(r'''\#\#\s*COLUMN=<
-            ID=(?P<id>.+),\s*
-            Type=(?P<type>.+),\s*
-            Description="(?P<descr>.*)"
-            >''', re.VERBOSE)
+column_pattern = re.compile(
+    r'''\#\#\s*COLUMN=<
+    ID=(?P<id>.+),\s*
+    Type=(?P<type>.+),\s*
+    Description="(?P<descr>.*)"
+    >''', 
+    re.VERBOSE
+)
+
+pedigree_pattern = re.compile(
+    r'''\#\#\s*PEDIGREE=<(?P<column>[^,]+)=(?P<node>[^,]+),\s*(?P<edges>.+)>''', 
+    re.VERBOSE
+)
+
+pedigree_edge_pattern = re.compile(r'''(?P<edge>[^,]+)=(?P<node>[^,]+)''')
 
 
 @dataclass(frozen=True)
@@ -56,6 +66,13 @@ class PedigreeHeader(object):
             node=self.node,
             edges=edges,
         )
+
+    @classmethod
+    def from_string(cls, string):
+        data = pedigree_pattern.match(string).groupdict()
+        edges = pedigree_edge_pattern.findall(data['edges'])
+        data['edges'] = [PedigreeEdge(e, n) for e, n in edges]
+        return cls(**data)
         
 
 @dataclass(frozen=True)
@@ -64,7 +81,7 @@ class PedigreeEdge(object):
     node: str
     
     def __str__(self):
-        return '{edge}={node}'.format(self.edge, self.node)
+        return '{edge}={node}'.format(edge=self.edge, node=self.node)
 
 
 TYPE_DISPATCH = {
@@ -77,12 +94,15 @@ TYPE_DISPATCH = {
 class BioTargetsHeader(object):
     meta: tuple
     columns: tuple
+    pedigree: tuple = ()
         
     def column_names(self):
         return [col.id for col in self.columns]
         
     def iter_lines(self):
         for line in self.meta:
+            yield str(line)
+        for line in self.pedigree:
             yield str(line)
         for line in self.columns:
             yield str(line)
@@ -113,6 +133,7 @@ def read_biotargets(path):
         lines = f.readlines()
     
     meta = list()
+    pedigree = list()
     columns= dict()  # order by colnames line
     
     for i, line in enumerate(lines):
@@ -120,6 +141,8 @@ def read_biotargets(path):
             if line.startswith('## COLUMN'):
                 obj = ColumnHeader.from_string(line)
                 columns[obj.id] = obj
+            elif line.startswith('## PEDIGREE'):
+                pedigree.append(PedigreeHeader.from_string(line.strip()))
             else:
                 meta.append(MetaHeader.from_string(line.strip()))
         else:
@@ -130,7 +153,8 @@ def read_biotargets(path):
     
     header = BioTargetsHeader(
         meta=tuple(meta),
-        columns=tuple(columns[col] for col in col_names)
+        columns=tuple(columns[col] for col in col_names),
+        pedigree=tuple(pedigree)
     )
     
     array = np.array([tuple(row.strip().split()) for row in lines[i:]], dtype=header.dtype())
