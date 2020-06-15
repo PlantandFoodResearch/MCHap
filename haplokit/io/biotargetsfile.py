070f888021f9399a91ae65fe03852d6e368b5472
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-06-15 15:50:03 +1200

    initial biotargetsfile classes

---
+++ haplokit/io/biotargetsfile.py
@@ -0,0 +1,138 @@
+import re
+import numpy as np
+from dataclasses import dataclass
+
+
+meta_pattern = re.compile(r'''\#\#\s*(?P<id>[^,]+)\s*=\s*(?P<descr>[^,]+)\s*''')
+
+column_pattern = re.compile(r'''\#\#\s*COLUMN=<
+            ID=(?P<id>.+),\s*
+            Type=(?P<type>.+),\s*
+            Description="(?P<descr>.*)"
+            >''', re.VERBOSE)
+
+
+@dataclass(frozen=True)
+class MetaHeader(object):
+    id: str
+    descr: str
+    
+    def __str__(self):
+        return '## {id}={descr}'.format(id=self.id, descr=self.descr)
+    
+    @classmethod
+    def from_string(cls, string):
+        data = meta_pattern.match(string).groupdict()
+        return cls(**data)
+    
+@dataclass(frozen=True)
+class ColumnHeader(object):
+    id: str
+    type: str
+    descr: str
+    
+    @classmethod
+    def from_string(cls, string):
+        data = column_pattern.match(string).groupdict()
+        return cls(**data)
+    
+    def __str__(self):
+        return '## COLUMN=<ID={id},Type={type},Description="{descr}">'.format(
+            id=self.id, 
+            type=self.type,
+            descr=self.descr,
+        )
+
+@dataclass(frozen=True)
+class PedigreeHeader(object):
+    column: str
+    node: str
+    edges: tuple # (PedigreeEdge, )
+        
+    def __str__(self):
+        edges = ','.join([str(edge for edge in self.edges)])
+        return '## PEDIGREE=<{column}={node},{edges}>'.format(
+            column=self.column,
+            node=self.node,
+            edges=edges,
+        )
+        
+
+@dataclass(frozen=True)
+class PedigreeEdge(object):
+    edge: str
+    node: str
+    
+    def __str__(self):
+        return '{edge}={node}'.format(self.edge, self.node)
+
+
+TYPE_DISPATCH = {
+    'integer': np.int,
+    'float': np.float,
+    'string': np.object,
+}
+
+@dataclass(frozen=True)
+class BioTargetsHeader(object):
+    meta: tuple
+    columns: tuple
+        
+    def column_names(self):
+        return [col.id for col in self.columns]
+        
+    def iter_lines(self):
+        for line in self.meta:
+            yield str(line)
+        for line in self.columns:
+            yield str(line)
+        yield '\t'.join(self.column_names())
+        
+    def __str__(self):
+        return '\n'.join(self.iter_lines())
+    
+    def dtype(self):
+        return np.dtype([(col.id, TYPE_DISPATCH[col.type.lower()]) for col in self.columns])
+        
+@dataclass
+class BioTargetsFile(object):
+    header: BioTargetsHeader
+    array: np.ndarray
+        
+    def iter_lines(self):
+        yield from self.header.iter_lines()
+        for row in self.array:
+            yield '\t'.join(map(str, row))
+        
+    def __str__(self):
+        return '\n'.join(self.iter_lines())
+    
+
+def read_biotargets(path):
+    with open(path) as f:
+        lines = f.readlines()
+    
+    meta = list()
+    columns= dict()  # order by colnames line
+    
+    for i, line in enumerate(lines):
+        if line.startswith('##'):
+            if line.startswith('## COLUMN'):
+                obj = ColumnHeader.from_string(line)
+                columns[obj.id] = obj
+            else:
+                meta.append(MetaHeader.from_string(line.strip()))
+        else:
+            break
+
+    col_names = lines[i].strip().split()
+    i += 1
+    
+    header = BioTargetsHeader(
+        meta=tuple(meta),
+        columns=tuple(columns[col] for col in col_names)
+    )
+    
+    array = np.array([tuple(row.strip().split()) for row in lines[i:]], dtype=header.dtype())
+    
+    return BioTargetsFile(header, array)
