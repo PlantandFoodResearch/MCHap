632542800d421ff9a102f7e8715807117a66a1ec
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-06-19 11:36:25 +1200

    program class

--- haplokit/application/pedigree_plot.py
+++ haplokit/application/pedigree_plot.py
@@ -1,8 +1,10 @@
+import os
 import numpy as np
 import networkx as nx
 import graphviz as gv
 import pysam
 import warnings
+from dataclasses import dataclass
 
 from haplokit.io.biotargetsfile import read_biotargets
 
@@ -52,15 +54,75 @@ def _genotype_string(array, symbol=True):
     return head + body + tail
 
 
-def as_haplotype_graphviz(
-        graph, 
+@dataclass
+class program(object):
+    digraph: nx.DiGraph
+    variants: list
+    sample_map: dict = None
+    default_ploidy: int = 2
+    label: str = 'label'
+    simplify_haplotypes: bool = False
+    transpose_haplotypes: bool = False
+    show_sample_names: bool = False
+    variant_names: bool = False
+    nodesep: int = 1
+    ranksep: int = 1
+    rankdir: str = 'TB'
+    output_format: str = 'pdf'
+    output_path: str = None
+
+    @classmethod
+    def cli(cls):
+        pass
+
+    def iter_graphs(self):
+        for variant in self.variants:
+            if self.variant_names and variant.id:
+                name = variant.id
+            else:
+                name = '{}_{}'.format(
+                    variant.chrom,
+                    variant.pos
+                )
+                if variant.stop:
+                    name = '{}_{}'.format(
+                        name,
+                        variant.stop,
+                    )
+
+            graph = graph_variant(
+                digraph=self.digraph, 
+                variant=variant, 
+                sample_map=self.sample_map, 
+                default_ploidy=self.default_ploidy, 
+                label=self.label, 
+                simplify_haplotypes=self.simplify_haplotypes,
+                transpose_haplotypes=self.transpose_haplotypes, 
+                show_sample_names=self.show_sample_names,
+                nodesep=self.nodesep,
+                ranksep=self.ranksep,
+                rankdir=self.rankdir,
+            )
+
+            yield name, graph
+
+    def run(self):
+        if not os.path.isdir(self.output_path):
+            raise IOError('{} is not a directory.'.format(self.output_path))
+        for name, graph in self.iter_graphs():
+            path = self.output_path + '/' + name + '.gv'
+            graph.render(path, format=self.output_format, cleanup=True)
+
+
+def graph_variant(
+        digraph, 
         variant, 
-        simplify=False,
         sample_map=None, 
         default_ploidy=2, 
         label='label', 
-        transpose=False, 
-        sample_names=False,
+        simplify_haplotypes=False,
+        transpose_haplotypes=False, 
+        show_sample_names=False,
         nodesep=1,
         ranksep=1,
         rankdir='TB'
@@ -69,7 +131,7 @@ def as_haplotype_graphviz(
     # map alleles to lists of chars
     haps = (variant.ref, ) + variant.alts
     positions = variant.info['VP']
-    if simplify:
+    if simplify_haplotypes:
         counts = [(pos, len({h[pos] for h in haps})) for pos in positions]
         positions = [pos for pos, count in counts if count > 1]
     alleles = {}
@@ -82,7 +144,7 @@ def as_haplotype_graphviz(
     ped_sample_arrays = {}
     for sample, record in variant.samples.items():
         array = np.array([alleles[a] for a in record['GT']])
-        if transpose:
+        if transpose_haplotypes:
             array = array.transpose()
         if sample_map:
             ped_item = sample_map[sample]
@@ -93,7 +155,7 @@ def as_haplotype_graphviz(
         ped_sample_arrays[ped_item][sample] = array
     
     # add null haps for pedigree items without sampels
-    for ped_item, data in graph.nodes(data=True):
+    for ped_item, data in digraph.nodes(data=True):
         # get ploidy if present
         if ped_item not in ped_sample_arrays:
             ploidy = data.get('ploidy')
@@ -102,7 +164,7 @@ def as_haplotype_graphviz(
                 ploidy = default_ploidy
             # create array of null haplotypes
             array = np.array([alleles[None] for _ in range(ploidy)])
-            if transpose:
+            if transpose_haplotypes:
                 array = array.transpose()
             ped_sample_arrays[ped_item]={'None': array}
     
@@ -115,7 +177,7 @@ def as_haplotype_graphviz(
     
     # get labels from graph (default to node name)
     labels = {}
-    for node, data in graph.nodes(data=True):
+    for node, data in digraph.nodes(data=True):
         labels[node] = data.get(label, node)
     
     # create subgraph of nodes for each sample
@@ -128,7 +190,7 @@ def as_haplotype_graphviz(
             for i, (sample, array) in enumerate(samples.items()):
                 name = '{}_{}'.format(node, i)
                 genotype = _genotype_string(array)
-                if sample_names:
+                if show_sample_names:
                     sub_cluster = '{}_{}'.format(cluster, sample)
                     with sg.subgraph(name=sub_cluster) as ssg:
                         ssg.attr(label=str(sample))
@@ -139,7 +201,7 @@ def as_haplotype_graphviz(
                     sg.node(name, genotype)
 
     # copy edges from initial graph
-    for parent, child in graph.edges():
+    for parent, child in digraph.edges():
         gvg.edge(
             '{}_{}'.format(parent, len(ped_sample_arrays[parent])//2), 
             '{}_{}'.format(child, len(ped_sample_arrays[child])//2), 
