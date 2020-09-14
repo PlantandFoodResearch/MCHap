5f9797d67d6c77b9e322427a97d9a3a397eb0713
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-09-15 11:35:14 +1200

    Test vcfstr

---
+++ test/test_io/test_vcf/test_vcf_util.py
@@ -0,0 +1,17 @@
+import pytest
+import numpy as np
+
+from haplokit.io.vcf import util
+
+@pytest.mark.parametrize('obj,expect', [
+    pytest.param(None, '.', id='None'),
+    pytest.param('', '.', id='empty-string'),
+    pytest.param(7, '7', id='integer'),
+    pytest.param((1, 2, 3), '1,2,3', id='tuple-integer'),
+    pytest.param([1, 2, 3], '1,2,3', id='list-integer'),
+    pytest.param(np.array([1, 2, 3]), '1,2,3', id='array-integer'),
+    pytest.param([1, None, 3], '1,.,3', id='list-mixed'),
+])
+def test_vcfstr(obj, expect):
+    actual = util.vcfstr(obj)
+    assert actual == expect
