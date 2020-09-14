b3551b8feb38f45cccf5d543ab791052a16d1782
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-09-14 14:32:36 +1200

    Test for output vcf header

--- test/test_denovo_assembly.py
+++ test/test_denovo_assembly.py
@@ -1,8 +1,11 @@
 import pathlib
+
+from haplokit.version import __version__
+from haplokit.io.vcf.headermeta import filedate
 from haplokit.application.denovo_assembly import program
 
 
-def test_Program__io():
+def test_Program__cli():
     samples = ('SAMPLE1', 'SAMPLE2', 'SAMPLE3')
 
     path = pathlib.Path(__file__).parent.absolute()
@@ -26,12 +29,17 @@ def test_Program__io():
         '--ref', REF,
         '--mcmc-steps', '500',
         '--mcmc-burn', '100',
+        '--seed', '11',
+        '--cores', '5',
     ]
 
     prog = program.cli(command)
 
     assert prog.mcmc_steps == 500
     assert prog.mcmc_burn == 100
+    assert prog.random_seed == 11
+    assert prog.n_cores == 5
+    assert prog.cli_command == command
 
     expect_sample_bam = dict(zip(samples, BAMS))
     assert len(prog.sample_bam) == len(expect_sample_bam)
@@ -44,9 +52,7 @@ def test_Program__io():
         assert prog.sample_ploidy[k] == v
 
 
-def test_Program__run():
-    samples = ('SAMPLE1', 'SAMPLE2', 'SAMPLE3')
-
+def test_Program__header():
     path = pathlib.Path(__file__).parent.absolute()
     path = path / 'test_io/data'
 
@@ -68,27 +74,47 @@ def test_Program__run():
         '--ref', REF,
         '--mcmc-steps', '500',
         '--mcmc-burn', '100',
+        '--seed', '11',
     ]
 
     prog = program.cli(command)
-    out = prog.run()
+    header = prog.header()
 
-    # check header
+    meta_expect = [
+        '##fileformat=VCFv4.3',
+        str(filedate()),
+        '##source=Haplokit v{}'.format(__version__),
+        '##phasing=None',
+        '##commandline="{}"'.format(' '.join(command)),
+        '##randomseed=11',
+    ]
+    meta_actual = [str(i) for i in header.meta]
+    assert meta_actual == meta_expect
+
     contigs_expect = [
         '##contig=<ID=CHR1,length=60>',
         '##contig=<ID=CHR2,length=60>',
         '##contig=<ID=CHR3,length=60>',
     ]
-    contigs_actual = [str(i) for i in out.header.contigs]
+    contigs_actual = [str(i) for i in header.contigs]
     assert contigs_actual == contigs_expect
 
+    filters_expect = [
+        r'##FILTER=<ID=k3<0.95,Description="Less than 95.0 % of samples read-variant 3-mers ">',
+        r'##FILTER=<ID=dp<5.0,Description="Sample has mean read depth less than 5.0.">',
+        r'##FILTER=<ID=rc<5.0,Description="Sample has read (pair) count of less than 5.0 in haplotype interval.">',
+        r'##FILTER=<ID=pp<0.95,Description="Samples phenotype posterior probability < 0.95.">',
+    ]
+    filters_actual = [str(i) for i in header.filters]
+    assert filters_actual == filters_expect
+
+
+    samples_expect = ('SAMPLE1', 'SAMPLE2', 'SAMPLE3')
+    samples_actual = header.samples
+    assert samples_actual == samples_expect
+
     columns_expect = ('CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT')
-    columns_expect += samples
-    columns_actual = out.header.columns()
+    columns_expect += samples_expect
+    columns_actual = header.columns()
     assert columns_expect == columns_actual
-
-    # check lines
-    assert len(out.records) == 3
-
-
 
