57961d17ee0defb1536066ed6db5afa69c661816
Timothy Millar <tim.millar@plantandfood.co.nz>
2020-12-17 12:55:43 +1300

    Use log-probs in chain swap step

--- mchap/assemble/mcmc/step/chain.py
+++ mchap/assemble/mcmc/step/chain.py
@@ -5,36 +5,34 @@ from mchap.assemble import util
 
 
 @numba.njit
-def genotype_prior(genotype, unique_haplotypes):
+def log_genotype_prior(genotype, unique_haplotypes):
     ploidy, _ = genotype.shape
     dosage = np.zeros(ploidy, dtype=np.int8)
     util.get_dosage(dosage, genotype)
     n_perms = util.count_equivalent_permutations(dosage)
     total_perms = unique_haplotypes ** ploidy
-    return n_perms / total_perms
+    return np.log(n_perms) - np.log(total_perms)
 
 
 @numba.njit
 def chain_swap_acceptance(
     genotype_i,
     llk_i,
-    prior_i,
+    log_prior_i,
     temp_i,
     genotype_j,
     llk_j,
-    prior_j,
+    log_prior_j,
     temp_j,
 ):
-    lk_i = np.exp(llk_i)
-    lk_j = np.exp(llk_j)
 
-    unnormalized_posterior_i = lk_i * prior_i
-    unnormalized_posterior_j = lk_j * prior_j
+    unnormalized_posterior_i = llk_i + log_prior_i
+    unnormalized_posterior_j = llk_j + log_prior_j
 
-    frac_1 = (unnormalized_posterior_j / unnormalized_posterior_i) ** temp_i
-    frac_2 = (unnormalized_posterior_i / unnormalized_posterior_j) ** temp_j
+    frac_1 = (unnormalized_posterior_j - unnormalized_posterior_i) * temp_i
+    frac_2 = (unnormalized_posterior_i - unnormalized_posterior_j) * temp_j
 
-    acceptance_ratio = frac_1 * frac_2
+    acceptance_ratio = np.exp(frac_1 + frac_2)
     if acceptance_ratio > 1.0:
         acceptance_ratio = 1.0
     
@@ -51,8 +49,8 @@ def chain_swap_step(
     temp_j,
     unique_haplotypes,
 ):
-    prior_i = genotype_prior(genotype_i, unique_haplotypes)
-    prior_j = genotype_prior(genotype_j, unique_haplotypes)
+    prior_i = log_genotype_prior(genotype_i, unique_haplotypes)
+    prior_j = log_genotype_prior(genotype_j, unique_haplotypes)
 
     acceptance = chain_swap_acceptance(
         genotype_i,
