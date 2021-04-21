c022614f12d8245fe1b66386291ecd0d2b6701fe
Timothy Millar <tim.millar@plantandfood.co.nz>
2021-04-21 16:49:26 +1200

    Better string convertion for large numpy arrays

--- mchap/tests/test_io/test_vcf/test_vcf_util.py
+++ mchap/tests/test_io/test_vcf/test_vcf_util.py
@@ -8,6 +8,7 @@ from mchap.io.vcf import util
     "obj,expect",
     [
         pytest.param(None, ".", id="None"),
+        pytest.param(np.nan, ".", id="nan"),
         pytest.param("", ".", id="empty-string"),
         pytest.param(7, "7", id="integer"),
         pytest.param((1, 2, 3), "1,2,3", id="tuple-integer"),
@@ -15,6 +16,9 @@ from mchap.io.vcf import util
         pytest.param(np.array([1, 2, 3]), "1,2,3", id="array-integer"),
         pytest.param([1, None, 3], "1,.,3", id="list-mixed"),
         pytest.param([1.3, 0.7, 1.0], "1.3,0.7,1", id="list-floats"),
+        pytest.param(
+            np.array([0.0321, np.nan, 1.0, 0.0]), "0.0321,.,1,0", id="array-floats"
+        ),
     ],
 )
 def test_vcfstr(obj, expect):
