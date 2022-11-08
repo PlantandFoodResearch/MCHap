d8e09838f4db6c02a5e79a22905f8ed74239b5c5
Timothy Millar <tim.millar@plantandfood.co.nz>
2022-11-08 14:27:22 +1300

    Remove python 3.7 and add 3.10

--- setup.py
+++ setup.py
@@ -40,9 +40,9 @@ setup(
         "Intended Audience :: Science/Research",
         "Natural Language :: English",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
 )
