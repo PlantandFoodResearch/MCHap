Fix integer overflow bug when calculating the total number of haplotype #157
- Use log_unique_haplotypes in place of unique_haplotypes
- Improvements to SNP-calling code performance

mchap/assemble/mutation.py
@@ -18,9 +18,9 @@ def base_step(
     llk,
     h,
     j,
-    unique_haplotypes,
+    n_alleles,
+    log_unique_haplotypes,
     inbreeding=0,
-    n_alleles=None,
     temp=1,
     read_counts=None,
     cache=None,
@@ -43,12 +43,12 @@ def base_step(
         Index of the haplotype to be mutated.
     j : int
         Index of the base position to be mutated
-    unique_haplotypes : int
-        Total number of unique haplotypes possible at this locus.
-    inbreeding : float
-        Expected inbreeding coefficient of the genotype.
     n_alleles : int
         Number of possible base alleles at this positions.
+    log_unique_haplotypes : float
+        The log of the total number of possible haplotypes.
+    inbreeding : float
+        Expected inbreeding coefficient of the genotype.
     temp : float
         An inverse temperature in the interval 0, 1 to adjust
         the sampled distribution by.
@@ -73,9 +73,6 @@ def base_step(
     """
     assert 0 <= temp <= 1
     ploidy = len(genotype)
-    # number of possible alleles given given array size
-    if n_alleles is None:
-        n_alleles = reads.shape[-1]
 
     # store log likelihoods calculated with each allele
     llks = np.empty(n_alleles)
@@ -90,7 +87,11 @@ def base_step(
     # ratio of prior probabilities
     dosage = np.empty(ploidy, dtype=np.int8)
     get_haplotype_dosage(dosage, genotype)
-    lprior = log_genotype_prior(dosage, unique_haplotypes, inbreeding)
+    lprior = log_genotype_prior(
+        dosage=dosage,
+        log_unique_haplotypes=log_unique_haplotypes,
+        inbreeding=inbreeding,
+    )
 
     current_nucleotide = genotype[h, j]
     n_options = 0
@@ -117,7 +118,11 @@ def base_step(
 
             # calculate ratio of priors: ln(P(G')/P(G))
             get_haplotype_dosage(dosage, genotype)
-            lprior_i = log_genotype_prior(dosage, unique_haplotypes, inbreeding)
+            lprior_i = log_genotype_prior(
+                dosage=dosage,
+                log_unique_haplotypes=log_unique_haplotypes,
+                inbreeding=inbreeding,
+            )
             lprior_ratio = lprior_i - lprior
 
             # calculate proposal ratio for detailed balance: ln(g(G|G')/g(G'|G))
@@ -152,8 +157,9 @@ def compound_step(
     genotype,
     reads,
     llk,
+    n_alleles,
+    log_unique_haplotypes,
     inbreeding=0,
-    n_alleles=None,
     temp=1,
     read_counts=None,
     cache=None,
@@ -171,10 +177,12 @@ def compound_step(
     llk : float
         Log-likelihood of the initial haplotype state given the
         observed reads.
-    inbreeding : float
-        Expected inbreeding coefficient of the genotype.
     n_alleles : ndarray, int, shape (n_base, )
         The number of possible alleles at each base position.
+    log_unique_haplotypes : float
+        The log of the total number of possible haplotypes.
+    inbreeding : float
+        Expected inbreeding coefficient of the genotype.
     temp : float
         An inverse temperature in the interval 0, 1 to adjust
         the sampled distribution by.
@@ -198,14 +206,6 @@ def compound_step(
     """
     ploidy, n_base = genotype.shape
 
-    if n_alleles is None:
-        max_allele = reads.shape[-1]
-        n_alleles = np.empty(n_base, dtype=np.int8)
-        n_alleles[:] = max_allele
-        unique_haplotypes = n_base**max_allele
-    else:
-        unique_haplotypes = np.prod(n_alleles)
-
     # matrix of haplotype-base combinations
     substeps = np.empty((ploidy * n_base, 2), dtype=np.int8)
 
@@ -227,7 +227,7 @@ def compound_step(
             h=h,
             j=j,
             cache=cache,
-            unique_haplotypes=unique_haplotypes,
+            log_unique_haplotypes=log_unique_haplotypes,
             inbreeding=inbreeding,
             n_alleles=n_alleles[j],
             temp=temp,

mchap/assemble/snpcalling.py
@@ -1,23 +1,25 @@
 import numpy as np
-from itertools import combinations_with_replacement
-from mchap.combinatorics import count_unique_genotypes
+from numba import njit
 from mchap.assemble.likelihood import log_likelihood
-from mchap.assemble.prior import log_genotype_prior
-from mchap.jitutils import normalise_log_probs
+from mchap.jitutils import (
+    normalise_log_probs,
+    comb_with_replacement,
+    increment_genotype,
+)
+from mchap.calling.prior import log_genotype_prior as log_snp_prior
 
 __all__ = ["snp_posterior"]
 
 
-def snp_posterior(reads, position, n_alleles, ploidy, inbreeding=0, read_counts=None):
+@njit(cache=True)
+def snp_posterior(read_probs, n_alleles, ploidy, inbreeding=0, read_counts=None):
     """Brute-force the posterior probability across all possible
     genotypes for a single SNP position.
 
     Parameters
     ----------
-    reads : ndarray, float, shape (n_reads, n_positions, max_allele)
-        Reads encoded as probability distributions.
-    position : int
-        Position of target SNP within reads.
+    read_probs : ndarray, float, shape (n_reads, n_positions, max_allele)
+        Reads probabilities at the given SNP.
     n_alleles : int
         Number of possible alleles for this SNP.
     ploidy : int
@@ -35,33 +37,30 @@ def snp_posterior(reads, position, n_alleles, ploidy, inbreeding=0, read_counts=
         Probability of each genotype.
 
     """
-    n_reads, n_pos, max_allele = reads.shape
+    n_reads, max_allele = read_probs.shape
     if n_reads == 0:
         # handle no reads
         n_reads = 1
-        reads = np.empty((n_reads, n_pos, max_allele), dtype=float)
-        reads[:] = np.nan
+        read_probs = np.empty((n_reads, max_allele), dtype=np.float64)
+        read_probs[:] = np.nan
 
-    u_gens = count_unique_genotypes(n_alleles, ploidy)
-    genotypes = np.zeros((u_gens, ploidy), dtype=np.int8) - 1
+    u_gens = comb_with_replacement(n_alleles, ploidy)
+    genotype = np.zeros(ploidy, dtype=np.int8)
+    genotypes = np.empty((u_gens, ploidy), dtype=np.int8)
     log_probabilities = np.empty(u_gens, dtype=float)
     log_probabilities[:] = -np.inf
-
-    alleles = np.arange(n_alleles)
-    for j, genotype in enumerate(combinations_with_replacement(alleles, ploidy)):
-        genotype = np.array(genotype)
-        genotypes[j] = genotype
-        _, dosage = np.unique(genotype, return_counts=True)
-        lprior = log_genotype_prior(dosage, n_alleles, inbreeding=inbreeding)
-        # treat as haplotypes with single position
+    for i in range(u_gens):
+        genotypes[i] = genotype
+        lprior = log_snp_prior(
+            genotype, unique_haplotypes=n_alleles, inbreeding=inbreeding
+        )
         llk = log_likelihood(
-            reads[:, position : position + 1, :],
-            genotype[..., None],
+            np.expand_dims(read_probs, 1),
+            np.expand_dims(genotype, -1),
             read_counts=read_counts,
         )
-
-        log_probabilities[j] = lprior + llk
-
+        log_probabilities[i] = lprior + llk
+        increment_genotype(genotype)
     # normalise
     probabilities = normalise_log_probs(log_probabilities)
     return genotypes, probabilities

mchap/assemble/structural.py
@@ -435,7 +435,7 @@ def interval_step(
     genotype,
     reads,
     llk,
-    unique_haplotypes,
+    log_unique_haplotypes,
     inbreeding=0,
     interval=None,
     step_type=0,
@@ -458,8 +458,8 @@ def interval_step(
     llk : float
         The log-likelihood of the current genotype state in the MCMC
         simulation.
-    unique_haplotypes : int
-        Total number of unique haplotypes possible at this locus.
+    log_unique_haplotypes : int
+        Log of the total number of unique haplotypes possible at this locus.
     inbreeding : float
         Expected inbreeding coefficient of the genotype.
     interval : ndarray, int, shape (2, )
@@ -509,7 +509,11 @@ def interval_step(
     ploidy = len(genotype)
     dosage = np.empty(ploidy, dtype=np.int8)
     get_haplotype_dosage(dosage, genotype)
-    lprior = log_genotype_prior(dosage, unique_haplotypes, inbreeding)
+    lprior = log_genotype_prior(
+        dosage=dosage,
+        log_unique_haplotypes=log_unique_haplotypes,
+        inbreeding=inbreeding,
+    )
 
     # store values for all options and current genotype
     llks = np.empty(n_options + 1)
@@ -533,7 +537,11 @@ def interval_step(
 
         # calculate ratio of priors: ln(P(G')/P(G))
         get_haplotype_dosage(dosage, option_labels[i])
-        lprior_i = log_genotype_prior(dosage, unique_haplotypes, inbreeding)
+        lprior_i = log_genotype_prior(
+            dosage=dosage,
+            log_unique_haplotypes=log_unique_haplotypes,
+            inbreeding=inbreeding,
+        )
         lprior_ratio = lprior_i - lprior
 
         # balance proposal distribution
@@ -578,10 +586,10 @@ def compound_step(
     reads,
     llk,
     intervals,
-    n_alleles=None,
+    log_unique_haplotypes,
     inbreeding=0,
     step_type=0,
-    randomise=True,
+    randomize=True,
     temp=1,
     read_counts=None,
     cache=None,
@@ -603,13 +611,13 @@ def compound_step(
         simulation.
     intervals : ndarray, int, shape (n_intervals, 2)
         The interval constraining each sub-step within this step.
-    n_alleles : ndarray, int, shape (n_base, )
-        The number of possible alleles at each base position.
+    log_unique_haplotypes : float
+        The log of the total number of possible haplotypes.
     inbreeding : float
         Expected inbreeding coefficient of the genotype.
     step_type : int
         0 for recombination or 1 for dosage swap.
-    randomise : bool, optional
+    randomize : bool, optional
         If True then the order of substeps (as defined by the
         order of intervals) will be randomly permuted
         (default = True).
@@ -635,26 +643,19 @@ def compound_step(
     The `genotype` variable is updated in place.
 
     """
-
     n_intervals = len(intervals)
 
-    if n_alleles is None:
-        _, n_base, max_allele = reads.shape
-        unique_haplotypes = max_allele**n_base
-    else:
-        unique_haplotypes = np.prod(n_alleles)
-
-    if randomise:
+    if randomize:
         intervals = intervals[np.random.permutation(np.arange(n_intervals))]
 
-    # step through every iterval
+    # step through every interval
     for i in range(n_intervals):
         llk, cache = interval_step(
             genotype=genotype,
             reads=reads,
             llk=llk,
             cache=cache,
-            unique_haplotypes=unique_haplotypes,
+            log_unique_haplotypes=log_unique_haplotypes,
             inbreeding=inbreeding,
             interval=intervals[i],
             step_type=step_type,