b81daa02f92ecd07118223225698528933f60f7d
Tim Millar <tim.millar@plantandfood.co.nz>
2024-05-10 21:51:23 +1200

    Update CLI help text

--- mchap/tests/test_docs.py
+++ mchap/tests/test_docs.py
@@ -4,7 +4,9 @@ import pathlib
 import subprocess
 
 
-@pytest.mark.skipif(sys.version_info != (3, 9), reason="argparse formatting changed")
+@pytest.mark.skipif(
+    sys.version_info[0:2] < (3, 11), reason="argparse formatting changed"
+)
 @pytest.mark.parametrize(
     "subtool",
     [
