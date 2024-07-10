4fcdbd59fa7b75bfd79521974b84d1d39f70f96b
Tim Millar <tim.millar@plantandfood.co.nz>
2024-07-11 11:01:58 +1200

    Additional tests for atomize

--- mchap/tests/test_application_atomize.py
+++ mchap/tests/test_application_atomize.py
@@ -3,8 +3,79 @@ import tempfile
 import os
 import sys
 import pytest
+import numpy as np
 
-from mchap.application.atomize import main
+from mchap.application.atomize import (
+    main,
+    format_snv_alleles,
+    get_haplotype_snv_indices,
+    format_allele_floats,
+)
+
+
+def test_format_snv_alleles():
+    haplotype_snvs = np.array(
+        [
+            ["A", "C", "A", "C"],
+            ["A", "C", "G", "C"],
+            ["A", "T", "T", "G"],
+        ]
+    )
+    refs, alts, n_alts = format_snv_alleles(haplotype_snvs)
+    np.testing.assert_array_equal(refs, ["A", "C", "A", "C"])
+    np.testing.assert_array_equal(alts, ["", "T", "G,T", "G"])
+    np.testing.assert_array_equal(n_alts, [0, 1, 2, 1])
+
+
+def test_get_haplotype_snv_indices():
+    haplotype_snvs = np.array(
+        [
+            ["A", "C", "A", "C"],
+            ["A", "C", "G", "C"],
+            ["A", "T", "T", "G"],
+        ]
+    )
+    actual = get_haplotype_snv_indices(haplotype_snvs)
+    expect = [
+        [0, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 1, 2, 1],
+    ]
+    np.testing.assert_array_equal(actual, expect)
+
+
+def test_format_allele_floats__R():
+    n_alts = np.array([1, 1, 2, 3])
+    floats = np.arange(60).reshape(5, 3, 4) / 15
+    floats[1] = np.nan
+    floats[3, 1] = [0.0, 1.0, np.nan, 1.1]
+    actual = format_allele_floats(floats, n_alts)
+    expect = np.array(
+        [
+            ["0,0.067", "0.267,0.333", "0.533,0.6"],
+            [".,.", ".,.", ".,."],
+            ["1.6,1.667,1.733", "1.867,1.933,2", "2.133,2.2,2.267"],
+            ["2.4,2.467,2.533,2.6", "0,1,.,1.1", "2.933,3,3.067,3.133"],
+        ]
+    )
+    np.testing.assert_array_equal(actual, expect)
+
+
+def test_format_allele_floats__A():
+    n_alts = np.array([1, 1, 2, 3])
+    floats = np.arange(60).reshape(5, 3, 4) / 15
+    floats[1] = np.nan
+    floats[3, 1] = [0.0, 1.0, np.nan, 1.1]
+    actual = format_allele_floats(floats[:, :, 1:], n_alts, length="A")
+    expect = np.array(
+        [
+            ["0.067", "0.333", "0.6"],
+            [".", ".", "."],
+            ["1.667,1.733", "1.933,2", "2.2,2.267"],
+            ["2.467,2.533,2.6", "1,.,1.1", "3,3.067,3.133"],
+        ]
+    )
+    np.testing.assert_array_equal(actual, expect)
 
 
 @pytest.mark.parametrize(
