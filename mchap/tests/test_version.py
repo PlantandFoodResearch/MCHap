e1a81cb084f411cedef90cb9a9406b5fe207629c
Tim Millar <tim.millar@plantandfood.co.nz>
2025-11-18 09:53:18 +1300

    Remove tomllib

--- mchap/tests/test_version.py
+++ mchap/tests/test_version.py
@@ -1,5 +1,4 @@
 import os
-import tomllib
 from pathlib import Path
 from mchap.version import __version__
 
@@ -8,6 +7,9 @@ def test_version():
     path = Path(os.path.dirname(os.path.abspath(__file__)))
     path = path.parent.parent / "pyproject.toml"
     with open(path.resolve(), "rb") as f:
-        data = tomllib.load(f)
-    toml_version = data["project"]["version"]
+        for line in f.readlines():
+            line = line.decode()
+            if line.startswith("version"):
+                toml_version = line.split('"')[1]
+                break
     assert __version__ == "v" + toml_version
